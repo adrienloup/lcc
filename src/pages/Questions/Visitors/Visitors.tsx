e9e3a43a58d3c5809e0f404fc60685f4a29fb5cf
Adrien Loup <adloup@gmail.com>
2023-04-23 04:57:45 +0200

    Add visitors page

---
+++ src/pages/Questions/Visitors/Visitors.tsx
@@ -0,0 +1,41 @@
+// import { resources } from '../../datas/resources.json'
+import { useNavigate } from 'react-router-dom'
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+import Button from '../../../components/Button/Button'
+// import './Resources.scss'
+
+function Visitors({ mode }: { mode: string }) {
+  const navigate = useNavigate()
+
+  return (
+    <div className={`question ${mode}`}>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <div className='inner'>
+          <h1 className='question_title'>Quel est le nombre de visiteurs par mois ?</h1>
+          <nav className='buttons'>
+            <Button
+              label='Retour'
+              cssClass='button'
+              ariaLabel={'Retourner à la page précédente'}
+              click={() => navigate(-1)}
+            />
+            <Button
+              label='Continuer'
+              cssClass='button'
+              ariaLabel='Continuer le parcours'
+              to={`/lcc/question/temps-moyen`}
+            />
+          </nav>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Visitors
