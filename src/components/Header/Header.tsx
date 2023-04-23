841767cbde5998db08d4455bf865eb15140c37c0
Adrien Loup <adloup@gmail.com>
2023-04-23 04:59:45 +0200

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import { FC, useContext } from 'react'
+import { Mode } from '../../contexts/mode'
+import { Path } from '../../utils/path'
+import Button from '../Button/Button'
+import './Header.scss'
+
+const Header: FC = () => {
+  const { mode, setMode } = useContext(Mode)
+
+  const handleMode = () => {
+    const isDark = mode === 'dark'
+    setMode(isDark ? 'light' : 'dark')
+    localStorage.setItem('mode', isDark ? 'light' : 'dark')
+  }
+
+  return (
+    <header
+      className='header'
+      role='banner'
+    >
+      <Button
+        cssClass='link'
+        label='LCC'
+        ariaLabel='Accéder à la page d’accueil'
+        to={Path}
+      />
+      <Button
+        cssClass='switch'
+        icon={mode === 'dark' ? 'moon' : 'sun'}
+        ariaLabel={mode === 'dark' ? 'Naviguer en mode clair' : 'Naviguer en mode sombre'}
+        click={() => handleMode()}
+      />
+    </header>
+  )
+}
+
+export default Header
