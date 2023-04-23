3af7272294c6f7b071ddd36fe5092ebb9798f2c0
Adrien Loup <adloup@gmail.com>
2023-04-23 04:59:33 +0200

    Add home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,30 @@
+import { Path } from '../../utils/path'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import Button from '../../components/Button/Button'
+import './Home.scss'
+
+function Home({ mode }: { mode: string }) {
+  return (
+    <div className={`${mode}`}>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <div className='inner'>
+          <h1 className='title'>Quel est l'impact de votre site web sur la planète ?</h1>
+          <Button
+            label='Démarrer'
+            cssClass='button'
+            ariaLabel='Démarrer le parcours'
+            to={`${Path}question/quel-parcours`}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
