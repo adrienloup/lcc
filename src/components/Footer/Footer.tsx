1d506bc276ecd3ccccd7da222c092423cefa0b4c
Adrien Loup <adloup@gmail.com>
2023-04-23 04:59:53 +0200

    Add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import Button from '../Button/Button'
+import './Footer.scss'
+
+function Footer() {  
+  return (
+    <footer
+      className='footer'
+      role='contentinfo'
+    >
+      <nav role='navigation'>
+        <ul>
+          <li>
+            <Button
+              label='v1.0.0'
+              cssClass='link'
+              ariaLabel='Voir la dernière version sur GitHub'
+              href='https://github.com/adrienloup/life-cycle-calculator'
+            />
+          </li>
+          <li>
+            <Button
+              label='Ressources'
+              cssClass='link'
+              ariaLabel='Voir les ressources utilisées pour le calcul'
+              to='/lcc/ressources'
+            />
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
+}
+
+export default Footer
