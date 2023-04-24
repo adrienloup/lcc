eefe1dad8d9a8b47641ccb1f5839a2bc5018eab3
Adrien Loup <adloup@gmail.com>
2023-04-24 21:24:42 +0200

    Add data context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import { Path } from './utils/path'
 import { Mode } from './contexts/mode'
+import { Data } from './contexts/data'
 import NotFound from './pages/NotFound/NotFound'
 import Resources from './pages/Resources/Resources'
 import Course from './pages/Questions/Course/Course'
@@ -16,37 +17,40 @@ const getMode = (): string => {
 
 function App() {
   const [mode, setMode] = useState(getMode())
+  const [data, setData] = useState({ q1: 'q1', q2: 'q2', q3: 'q3' })
 
   return (
     <Mode.Provider value={{ mode, setMode }}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path={Path}
-            element={<Home mode={mode}/>}
-          />
-          <Route
-            path={`${Path}ressources`}
-            element={<Resources mode={mode}/>}
-          />
-          <Route
-            path={`${Path}question/quel-parcours`}
-            element={<Course mode={mode}/>}
-          />
-          <Route
-            path={`${Path}question/nombre-de-visiteurs`}
-            element={<Visitors mode={mode}/>}
-          />
-          <Route
-            path={`${Path}question/temps-moyen`}
-            element={<Duration mode={mode}/>}
-          />
-          <Route
-            path='*'
-            element={<NotFound />}
-          /> 
-        </Routes>
-      </BrowserRouter>
+      <Data.Provider value={{ data, setData }}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path={Path}
+              element={<Home mode={mode}/>}
+            />
+            <Route
+              path={`${Path}ressources`}
+              element={<Resources mode={mode}/>}
+            />
+            <Route
+              path={`${Path}question/quel-parcours`}
+              element={<Course mode={mode}/>}
+            />
+            <Route
+              path={`${Path}question/nombre-de-visiteurs`}
+              element={<Visitors mode={mode}/>}
+            />
+            <Route
+              path={`${Path}question/temps-moyen`}
+              element={<Duration mode={mode}/>}
+            />
+            <Route
+              path='*'
+              element={<NotFound />}
+            /> 
+          </Routes>
+        </BrowserRouter>
+      </Data.Provider>
     </Mode.Provider>
   )
 }
