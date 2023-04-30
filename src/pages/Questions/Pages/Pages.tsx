d561a0ad51c444b2ee2b6c26e6ebb57b8a327af3
Adrien Loup <adloup@gmail.com>
2023-05-01 00:50:48 +0200

    Add pages page

---
+++ src/pages/Questions/Pages/Pages.tsx
@@ -0,0 +1,57 @@
+import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { Data } from '../../../contexts/data'
+import Information from '../../../components/Information/Information'
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+import Button from '../../../components/Button/Button'
+import Title from '../../../components/Title/Title'
+
+function Pages({ mode }: { mode: string }) {
+  const navigate = useNavigate()
+  const { data, setData } = useContext(Data)
+
+  const handleData = () => {
+    setData({ ...data, q6: 'answer6' })
+  }
+
+  return (
+    <div className={`question ${mode}`}>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <div onClick={handleData}>{data.q6}</div>
+        <Title
+          cssClass='title'
+          label='Nombre de pages consultées&nbsp;?'
+        />
+        <nav
+          className='navigation'
+          role='navigation'
+        >
+          <Button
+            label='Retour'
+            cssClass='button'
+            ariaLabel={'Retourner à la page précédente'}
+            click={() => navigate(-1)}
+          />
+          <Button
+            label='Continuer'
+            cssClass='button'
+            ariaLabel='Continuer le parcours'
+            to='/lcc/question/nombre-de-serveurs'
+          />
+        </nav>
+        <Information
+          cssClass='information'
+          label={'Les conditions d’utilisation de votre service numérique, en mobilité ou au bureau, ont des impacts bien différents.'}
+        />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Pages
