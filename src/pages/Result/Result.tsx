7b4bf86713f149ae3ca593cf9b2b783e09814c73
Adrien Loup <adloup@gmail.com>
2023-05-01 00:48:04 +0200

    Add result page

---
+++ src/pages/Result/Result.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import { Data } from '../../contexts/data'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import Button from '../../components/Button/Button'
+import Title from '../../components/Title/Title'
+import Tabs from '../../components/Tabs/Tabs'
+import './Result.scss'
+
+function Result({ mode }: { mode: string }) {
+  const { data } = useContext(Data)
+
+  return (
+    <div className={`result ${mode}`}>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <Title
+          cssClass='title'
+          label='Voici les impacts du parcours "lire une page de contenu", dans le but "de choisir une agence".'
+        />
+        <Tabs data={data} />
+        <Button
+          label='Recommencer'
+          cssClass='button'
+          role='button'
+          ariaLabel='Recommencer le parcours'
+          to='/lcc/'
+        />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Result
