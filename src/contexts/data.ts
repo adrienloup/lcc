3316cba9f11a65ea62b6f97e4db05dee5b5910ac
Adrien Loup <adloup@gmail.com>
2023-05-01 00:51:52 +0200

    Add data context

--- src/contexts/data.ts
+++ src/contexts/data.ts
@@ -1,6 +1,24 @@
 import { createContext } from 'react'
 
 export const Data = createContext({
-  data: { q1: 'q1', q2: 'q2', q3: 'q3' },
-  setData: (_data: any) => {},
+  data: {
+    q1: 'Site Web',
+    q2: '2500',
+    q3: '3',
+    q4: 'fr',
+    q5: '70',
+    q6: '5',
+    q7: '2',
+    q8: 'eu'
+  },
+  setData: (data: {
+    q1: string,
+    q2: string,
+    q3: string,
+    q4: string,
+    q5: string,
+    q6: string,
+    q7: string,
+    q8: string
+  }) => { data },
 })
