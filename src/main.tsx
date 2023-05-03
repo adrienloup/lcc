8185db900fee3d3ec8702d0f370c0ca5eee09f47
Adrien Loup <adloup@gmail.com>
2023-05-03 15:44:58 +0200

    Add strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/app.scss'
 console.log(
   '%c[LIFE CYCLE CALCULATOR]%cv1.0.0%c[@jff]',
   'padding: 4px 3px 3px; background: #1e1e20; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
-  'padding: 4px 3px 3px; background: #ee00ff; font-weight: bold; color: #1e1e20;',
+  'padding: 4px 3px 3px; background: #00f2c3; font-weight: bold; color: #1e1e20;',
   'padding: 4px 3px 3px; background: #1e1e20; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;'
 );
 
