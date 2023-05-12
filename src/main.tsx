94d126a25b1b676c9b11383d5397c41bbf0ae1cc
Adrien Loup <adloup@gmail.com>
2023-05-12 23:28:06 +0200

    Update version

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './styles/app.scss'
 
 console.log(
-  '%c[LIFE CYCLE CALCULATOR]%cv1.0.0%c[@jff]',
+  '%c[LIFE CYCLE CALCULATOR]%cv1.0.3%c[@jff]',
   'padding: 4px 3px 3px; background: #1e1e20; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
   'padding: 4px 3px 3px; background: #00f2c3; font-weight: bold; color: #1e1e20;',
   'padding: 4px 3px 3px; background: #1e1e20; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;'
