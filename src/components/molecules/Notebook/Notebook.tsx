113f7b6395cd348ffedbbea4cb740e09c61e2910
Adrien Loup <adloup@gmail.com>
2023-05-18 19:38:04 +0200

    Add notebook component

--- src/components/molecules/Notebook/Notebook.tsx
+++ src/components/molecules/Notebook/Notebook.tsx
@@ -3,30 +3,27 @@ import { NotebookProps } from '../../../models/notebook'
 import Button from '../Button/Button'
 import './Notebook.scss'
 
-function Notebook({ label }: NotebookProps) {
+function Notebook({ note }: NotebookProps) {
   const [ active, setActive ] = useState(false)
 
   return (
     <div className={`notebook${active ? ' active' : ''}`}>
       <Button
-        cssClass='tooltip'
-        label='i'
+        cssClass='alert'
+        icon='notebook'
         ariaLabel={active ? 'Fermer les notes' : 'Ouvrir les notes'}
         ariaExpanded={active}
         click={() => setActive(active => !active)}
       />
-      <div className='note'>
-        <div>
-          <Button
-            cssClass='close'
-            label='Fermer'
-            ariaLabel={active ? 'Fermer les notes' : 'Ouvrir les notes'}
-            ariaExpanded={active}
-            tabindex={active ? 0 : -1}
-            click={() => setActive(false)}
-          />
-          <p dangerouslySetInnerHTML={{__html: label}}></p>
-        </div>
+      <div>
+        <Button
+          label='Fermer'
+          ariaLabel={active ? 'Fermer les notes' : 'Ouvrir les notes'}
+          ariaExpanded={active}
+          tabindex={active ? 0 : -1}
+          click={() => setActive(false)}
+        />
+        <p>{note}</p>
       </div>
     </div>
   )
--- src/models/notebook.tsx
+++ src/models/notebook.tsx
@@ -1,3 +1,3 @@
 export interface NotebookProps {
-  label: string;
+  note: string;
 }
