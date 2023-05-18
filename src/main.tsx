a3d06104ecadd427f40befa6909fa01f0ef1f4b2
Adrien Loup <adloup@gmail.com>
2023-05-18 19:42:05 +0200

    Init project

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,13 @@ import './styles/app.scss'
 
 console.log(
   '%c[LIFE CYCLE CALCULATOR]%cv1.0.3%c[@jff]',
-  'padding: 4px 3px 3px; background: #1e1e20; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
-  'padding: 4px 3px 3px; background: #00f2c3; font-weight: bold; color: #1e1e20;',
-  'padding: 4px 3px 3px; background: #1e1e20; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;'
+  'padding: 4px 3px 3px; background: #32337b; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
+  'padding: 4px 3px 3px; background: #38dbf3; font-weight: bold; color: #32337b;',
+  'padding: 4px 3px 3px; background: #32337b; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;'
 );
 
 ReactDOM.createRoot(document.getElementById('lcc') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
