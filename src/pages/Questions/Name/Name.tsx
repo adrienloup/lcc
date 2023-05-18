825d29fa2aa131983b9d8caba225bc7d605e95b5
Adrien Loup <adloup@gmail.com>
2023-05-18 19:41:54 +0200

    Add main page

---
+++ src/pages/Questions/Name/Name.tsx
@@ -0,0 +1,66 @@
+import { useNavigate } from 'react-router-dom'
+import { useContext, useState } from 'react'
+import { DataContext } from '../../../contexts/data'
+import Notebook from '../../../components/molecules/Notebook/Notebook'
+import Scorebar from '../../../components/molecules/Scorebar/Scorebar'
+import Header from '../../../components/organisms/Header/Header'
+import Footer from '../../../components/organisms/Footer/Footer'
+import Button from '../../../components/molecules/Button/Button'
+import Title from '../../../components/atoms/Title/Title'
+
+function Name() {
+  const navigate = useNavigate()
+  const { data, setData } = useContext(DataContext)
+  const [ value, setValue ] = useState(data.name)
+
+  const handleChange = (e: { target: { value: string } }) => {
+    const name = e.target.value
+    setData({ ...data, name: name })
+    setValue(name)
+  }
+
+  const handleClick = () => {
+    localStorage.setItem('lcc-data', JSON.stringify(data))
+    navigate('/lcc/question/nombre-visiteurs')
+  }
+
+  return (
+    <div className='page'>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <Scorebar />
+        <article className='article'>
+          <Title title='1/8 De quel site Web mesure-t-on l’impact&nbsp;?' />
+          <Notebook note={'L’éco-conception part toujours d’un besoin utilisateur clairement identifié et dont le service numérique cherche à apporter une solution.'} />
+          <form className='form' action=''>
+            <label htmlFor='name'>Nom du site Web</label>
+            <input
+              id='name'
+              type='text'
+              maxLength={15}
+              value={value}
+              onChange={handleChange}
+            />
+          </form>
+          <nav
+            className='navigation'
+            role='navigation'
+          >
+            <Button
+              label='Je continue'
+              cssClass='button'
+              ariaLabel='Je ontinue le calcul'
+              click={() => handleClick()}
+            />
+          </nav>
+        </article>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Name
