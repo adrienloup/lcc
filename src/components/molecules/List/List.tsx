8f9d75e3c4b78331c52757d735951fabe130d721
Adrien Loup <adloup@gmail.com>
2023-05-25 19:43:23 +0200

    Initialize lcc project

--- src/components/molecules/List/List.tsx
+++ src/components/molecules/List/List.tsx
@@ -2,7 +2,7 @@ import { ListProps } from '../../../models/list'
 import './List.scss'
 
 function List({ cssClass, list }: ListProps) {
-  return <ul className={cssClass}>{list}</ul>
+  return <ul className={`list ${cssClass}`}>{list}</ul>
 }
 
 export default List
