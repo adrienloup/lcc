dbcc0cc64df2ade2176af768f1f83a9d4c9d2713
Adrien Loup <adloup@gmail.com>
2023-05-25 19:43:13 +0200

    Initialize lcc project

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Scorebar from '../../components/molecules/Scorebar/Scorebar'
+import Subtitle from '../../components/atoms/Subtitle/Subtitle'
 import Header from '../../components/organisms/Header/Header'
 import Footer from '../../components/organisms/Footer/Footer'
 import Button from '../../components/molecules/Button/Button'
@@ -18,15 +19,17 @@ function Home() {
           <div className='heading'>
             <Title text='Quel est l’impact de votre site Web<br />sur la planète&nbsp;?' />
           </div>
-          <Text text='<p>L’Analyse du Cycle de Vie (ACV), vous aide à éco-concevoir votre site Web zéro carbone.</p><p>Votre résultat évolue à chaque nouvelle réponse&nbsp;!</p><p>Chaque calcul est sauvegardé dans votre navigateur Web.<br />Vous êtes le seul à y avoir accès.</p>' />
+          <div className='frame'>
+            <Text text='<p>Le calculateur de cycle de vie est un outil d’éco-conception et d’Analyse de Cycle de Vie (ACV), qui vous aide à éco-concevoir votre site Web.</p><p>Répondez à 8 questions pour évaluer votre empreinte carbone. Votre résultat évolue à chaque nouvelle réponse. Vous pouvez modifier vos réponses pour améliorer le score. Chaque calcul est sauvegardé dans votre navigateur Web. Vous êtes le seul à y avoir accès.</p>' />
+          </div>
           <nav
             className='wrapper'
             role='navigation'
           >
             <Button
-              label='Je démarre !'
+              label='À l’action !'
               cssClass='start'
-              ariaLabel='Je démarre le calcul'
+              ariaLabel='À l’action pour commencer le calcul'
               to='/lcc/question/site-web'
             />
           </nav>
