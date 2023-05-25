7c8affa08e7f6e7338ffe563be0ab2d1e518f92a
Adrien Loup <adloup@gmail.com>
2023-05-25 19:43:35 +0200

    Initialize lcc project

--- src/pages/Documentation/Reference/Reference.tsx
+++ src/pages/Documentation/Reference/Reference.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react'
-import { resources } from '../../../datas/resources.json'
 import Heading from '../../../components/molecules/Heading/Heading'
 import Subtitle from '../../../components/atoms/Subtitle/Subtitle'
 import Header from '../../../components/organisms/Header/Header'
@@ -30,22 +29,6 @@ function Reference() {
     </li>
   ))
 
-  const resourceList = (list: {
-    label: string;
-    href: string;
-  }[]) => list.map((item, index) => (
-    <li
-      key={index}
-      className='item'
-    >
-      <Button
-        cssClass='link'
-        label={item.label}
-        href={item.href}
-      />
-    </li>
-  ))
-
   useEffect(() => {
     window.scrollTo(0,0)
   },[])
@@ -58,33 +41,17 @@ function Reference() {
         role='main'
       >
         <article className='article'>
-          <Heading text='Modèle de référence et documentation' />
-          <Text text='<p>Le calculateur de cycle de vie est un outil d’éco-conception et d’Analyse du Cycle de Vie (ACV) pour vous aider à éco-concevoir votre site Web zéro carbone.</p>' />
-          <Subtitle text='Les modèles de référence' />
-          <Text text='Explorez les modèles' />
+          <Heading text='Les modèles de référence' />
+          <Text text='<p>Le calculateur de cycle de vie évalue l’empreinte carbone annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
+          <Subtitle text='Comment l’empreinte carbone est-elle calculée&nbsp;?' />
           <List
             cssClass='models'
             list={modelList([
-              {label: 'Énergie primaire', ariaLabel: 'Afficher l’explication sur l’énergie primaire', icon: 'energy', to: '/lcc/documentation/energie-primaire'},
-              {label: 'Gaz à effet de serre', ariaLabel: 'Afficher l’explication sur le gaz à effet de serre', icon: 'cloud', to: '/lcc/documentation/gaz-a-effet-de-serre'},
               {label: 'Matières premières', ariaLabel: 'Afficher l’explication sur les matières premières', icon: 'materials', to: '/lcc/documentation/matieres-premieres'},
-              {label: 'Eau', ariaLabel: 'Afficher l’explication sur l’eau', icon: 'watter', to: '/lcc/documentation/eau'}
+              {label: 'Eau', ariaLabel: 'Afficher l’explication sur l’eau', icon: 'watter', to: '/lcc/documentation/eau'},
+              {label: 'Énergie primaire', ariaLabel: 'Afficher l’explication sur l’énergie primaire', icon: 'energy', to: '/lcc/documentation/energie-primaire'},
+              {label: 'Gaz à effet de serre', ariaLabel: 'Afficher l’explication sur le gaz à effet de serre', icon: 'cloud', to: '/lcc/documentation/gaz-a-effet-de-serre'}
             ])}
-          />
-          <Subtitle text='Documentation générale' />
-          <List
-            cssClass='resources'
-            list={resourceList(resources[0]['list'])}
-          />
-          <Subtitle text='Impacts du numérique' />
-          <List
-            cssClass='resources'
-            list={resourceList(resources[1]['list'])}
-          />
-          <Subtitle text='Outils d’audit' />
-          <List
-            cssClass='resources'
-            list={resourceList(resources[2]['list'])}
           />
         </article>
       </main>
