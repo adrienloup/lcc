d15be92be472521f67f5724c14f309e0ce7336a4
Adrien Loup <adloup@gmail.com>
2023-05-26 19:00:57 +0200

    Initialize lcc project

---
+++ src/pages/A11y/A11y.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import Header from '../../components/organisms/Header/Header'
+import Footer from '../../components/organisms/Footer/Footer'
+import Title from '../../components/atoms/Title/Title'
+
+function A11y() {
+
+  useEffect(() => {
+    window.scrollTo(0,0)
+  },[])
+  
+  return (
+    <div className='page'>
+      <Header />
+      <main
+        className='main'
+        role='main'
+      >
+        <article className='article'>
+          <div className='heading'>
+            <Title text='Déclaration d’accessibilité numérique' />
+          </div>
+          @TODO
+        </article>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default A11y
