745e79ad3eb8f757f35a8e6a69bab2a848782fbb
Adrien Loup <adloup@gmail.com>
2023-05-26 19:01:40 +0200

    Initialize lcc project

--- src/pages/Questions/User/User.tsx
+++ src/pages/Questions/User/User.tsx
@@ -6,17 +6,18 @@ import Scorebar from '../../../components/molecules/Scorebar/Scorebar'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
+import Back from '../../../components/molecules/Back/Back'
 import Title from '../../../components/atoms/Title/Title'
 
-function Visitor() {
+function User() {
   const navigate = useNavigate()
   const { data, setData } = useContext(DataContext)
-  const [ value, setValue ] = useState(data.visitor)
+  const [ value, setValue ] = useState(data.user)
 
   const handleChange = (e: { target: { value: string } }) => {
-    const visitor = Number(e.target.value)
-    setData({ ...data, visitor: visitor })
-    setValue(visitor)
+    const user = Number(e.target.value)
+    setData({ ...data, user: user })
+    setValue(user)
     localStorage.setItem('lcc-data', JSON.stringify(data))
   }
 
@@ -34,35 +35,34 @@ function Visitor() {
       >
         <Scorebar />
         <article className='article'>
-          <Title text='2/8 Quel est le nombre de visiteurs par mois&nbsp;?' />
+          <div className='heading'>
+            <Title text='<span>2/8</span> Quel est le nombre de visiteurs par mois&nbsp;?' />
+          </div>
           <Notebook note={'Le nombre de visiteurs par mois permet de connaître la quantité de teminaux utilisateur, équipements réseau, serveur nécessaire pour consulter les contenus ou les services que vous délivrez. Si vous ne la connaissez pas, vous pouvez trouver cette info dans votre outil Google Analytics ou équivalent.'} />
-          <form className='form' action=''>
-            <label htmlFor='visitor'>{value} visiteurs</label>
-            <input
-              id='visitor'
-              type='range'
-              min='1000'
-              max='10000'
-              step='1'
-              style={{backgroundSize: `${(value * 100) / 10000}% 100%`}}
-              value={value}
-              onChange={handleChange}
-            />
-          </form>
+          <div className='frame'>
+            <form className='form' action=''>
+              <label htmlFor='user'>{value} visiteurs</label>
+              <input
+                id='user'
+                type='range'
+                min='1000'
+                max='10000'
+                step='1'
+                style={{backgroundSize: `${(value * 100) / 10000}% 100%`}}
+                value={value}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
           <nav
-            className='navigation'
+            className='footing'
             role='navigation'
           >
+            <Back />
             <Button
-              label='Retour'
+              label='Je continue'
               cssClass='button'
-              ariaLabel={'Modifier la réponse précédente'}
-              click={() => navigate(-1)}
-            />
-            <Button
-              label='Continuer'
-              cssClass='button'
-              ariaLabel='Continuer le calcul'
+              ariaLabel='Je continue le calcul'
               click={() => handleClick()}
             />
           </nav>
@@ -73,4 +73,4 @@ function Visitor() {
   )
 }
 
-export default Visitor
+export default User
