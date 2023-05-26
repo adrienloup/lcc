1785ecd8788875c8a7bc2b400ddbf977e1c8e2d1
Adrien Loup <adloup@gmail.com>
2023-05-26 19:01:13 +0200

    Initialize lcc project

---
+++ src/utils/coin.tsx
@@ -0,0 +1,14 @@
+function coin(
+  user: number,
+  duration: number,
+  mobile: number,
+  server: number,
+) {
+  return Math.round((
+    (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (572 / 5) + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * (572 / 5) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (1689 / 5)
+    + (((user * 12 * duration) / 60) / (24 * 365)) * (425 / 5)
+    + server * (2288 / 5)
+  ) / (user * 12) / 0.0075)
+}
+
+export default coin
