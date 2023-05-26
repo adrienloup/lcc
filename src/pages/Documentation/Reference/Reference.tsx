6f7e376b70583b0f5f6e466dc403e7cad559cf35
Adrien Loup <adloup@gmail.com>
2023-05-26 19:02:16 +0200

    Initialize lcc project

--- src/pages/Documentation/Reference/Reference.tsx
+++ src/pages/Documentation/Reference/Reference.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { models } from '../../../datas/models.json'
 import Heading from '../../../components/molecules/Heading/Heading'
 import Subtitle from '../../../components/atoms/Subtitle/Subtitle'
 import Header from '../../../components/organisms/Header/Header'
@@ -9,7 +10,7 @@ import Text from '../../../components/atoms/Text/Text'
 
 function Reference() {
 
-  const modelList = (list: {
+  const list = (list: {
     label: string;
     ariaLabel: string;
     icon: string;
@@ -42,16 +43,11 @@ function Reference() {
       >
         <article className='article'>
           <Heading text='Les modèles de référence' />
-          <Text text='<p>Le calculateur de cycle de vie évalue l’empreinte carbone annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
-          <Subtitle text='Comment l’empreinte carbone est-elle calculée&nbsp;?' />
+          <Text text='<p>Le calculateur de cycle de vie évalue l’impact annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
+          <Subtitle text='Comment l’impact écologique est-il calculé&nbsp;?' />
           <List
             cssClass='models'
-            list={modelList([
-              {label: 'Matières premières', ariaLabel: 'Afficher l’explication sur les matières premières', icon: 'materials', to: '/lcc/documentation/matieres-premieres'},
-              {label: 'Eau', ariaLabel: 'Afficher l’explication sur l’eau', icon: 'watter', to: '/lcc/documentation/eau'},
-              {label: 'Énergie primaire', ariaLabel: 'Afficher l’explication sur l’énergie primaire', icon: 'energy', to: '/lcc/documentation/energie-primaire'},
-              {label: 'Gaz à effet de serre', ariaLabel: 'Afficher l’explication sur le gaz à effet de serre', icon: 'cloud', to: '/lcc/documentation/gaz-a-effet-de-serre'}
-            ])}
+            list={list(models)}
           />
         </article>
       </main>
