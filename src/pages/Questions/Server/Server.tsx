cade88f24e31133edd31b8677236538de0e3e2e7
Adrien Loup <adloup@gmail.com>
2023-05-26 19:01:59 +0200

    Initialize lcc project

--- src/pages/Questions/Server/Server.tsx
+++ src/pages/Questions/Server/Server.tsx
@@ -6,6 +6,7 @@ import Scorebar from '../../../components/molecules/Scorebar/Scorebar'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
+import Back from '../../../components/molecules/Back/Back'
 import Title from '../../../components/atoms/Title/Title'
 
 function Server() {
@@ -34,35 +35,34 @@ function Server() {
       >
         <Scorebar />
         <article className='article'>
-          <Title text='7/8 Quel est le nombre de serveurs utilisés&nbsp;?' />
+          <div className='heading'>
+            <Title text='7/8 Quel est le nombre de serveurs utilisés&nbsp;?' />
+          </div>
           <Notebook note={'Calculé à partir d’un PUE (Power Usage Effectiveness) moyen d’un serveur (soit 1,8). Si vous avez opté pour un hébergement « green », vous serez meilleur que la moyenne du marché sur ce paramètre. Compter les serveurs de production, staging, review, CDN, etc...'} />
-          <form className='form' action=''>
-            <label htmlFor='serveur'>{value} {value > 1 ? 'serveurs' : 'serveur'}</label>
-            <input
-              id='serveur'
-              type='range'
-              min='1'
-              max='5'
-              step='1'
-              style={{backgroundSize: `${(value * 100) / 5}% 100%`}}
-              value={value}
-              onChange={handleChange}
-            />
-          </form>
+          <div className='frame'>
+            <form className='form' action=''>
+              <label htmlFor='serveur'>{value} {value > 1 ? 'serveurs' : 'serveur'}</label>
+              <input
+                id='serveur'
+                type='range'
+                min='1'
+                max='5'
+                step='1'
+                style={{backgroundSize: `${(value * 100) / 5}% 100%`}}
+                value={value}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
           <nav
-            className='navigation'
+            className='footing'
             role='navigation'
           >
+            <Back />
             <Button
-              label='Retour'
+              label='Je continue'
               cssClass='button'
-              ariaLabel={'Retour à la question précédente'}
-              click={() => navigate(-1)}
-            />
-            <Button
-              label='Continuer'
-              cssClass='button'
-              ariaLabel='Continuer le calcul'
+              ariaLabel='Je continue le calcul'
               click={() => handleClick()}
             />
           </nav>
