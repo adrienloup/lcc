dd5b2ece652cffc80b225fdb7f438a3c8402adf8
Adrien Loup <adloup@gmail.com>
2023-05-26 19:01:23 +0200

    Initialize lcc project

--- src/pages/Questions/Duration/Duration.tsx
+++ src/pages/Questions/Duration/Duration.tsx
@@ -6,6 +6,7 @@ import Scorebar from '../../../components/molecules/Scorebar/Scorebar'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
+import Back from '../../../components/molecules/Back/Back'
 import Title from '../../../components/atoms/Title/Title'
 
 function Duration() {
@@ -19,6 +20,11 @@ function Duration() {
     setValue(duration)
   }
 
+  const handleClick = () => {
+    localStorage.setItem('lcc-data', JSON.stringify(data))
+    navigate('/lcc/question/localisation-utilisateurs')
+  }
+
   return (
     <div className='page'>
       <Header />
@@ -28,36 +34,35 @@ function Duration() {
       >
         <Scorebar />
         <article className='article'>
-          <Title text='3/8 Quel est le temps passé en moyenne par utilisateur&nbsp;?' />
+          <div className='heading'>
+            <Title text='<span>3/8</span> Quel est le temps passé en moyenne par utilisateur&nbsp;?' />
+          </div>
           <Notebook note={'Le temps que les utilisateurs passent sur votre service numérique permet de connaître la quantité de teminaux, équipements réseau, serveur nécessaire pour servir, transiter et consulter les contenus ou les services que vous délivrez.'} />
-          <form className='form' action=''>
-            <label htmlFor='duration'>{value} {value > 1 ? 'minutes' : 'minute'}</label>
-            <input
-              id='duration'
-              type='range'
-              min='1'
-              max='20'
-              step='1'
-              style={{backgroundSize: `${(value * 100) / 20}% 100%`}}
-              value={value}
-              onChange={handleChange}
-            />
-          </form>
+          <div className='frame'>
+            <form className='form' action=''>
+              <label htmlFor='duration'>{value} {value > 1 ? 'minutes' : 'minute'}</label>
+              <input
+                id='duration'
+                type='range'
+                min='1'
+                max='20'
+                step='1'
+                style={{backgroundSize: `${(value * 100) / 20}% 100%`}}
+                value={value}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
           <nav
-            className='navigation'
+            className='footing'
             role='navigation'
           >
+            <Back />
             <Button
-              label='Retour'
+              label='Je continue'
               cssClass='button'
-              ariaLabel={'Retour à la question précédente'}
-              click={() => navigate(-1)}
-            />
-            <Button
-              label='Continuer'
-              cssClass='button'
-              ariaLabel='Continuer le calcul'
-              to='/lcc/question/localisation-utilisateurs'
+              ariaLabel='Je continue le calcul'
+              click={() => handleClick()}
             />
           </nav>
         </article>
