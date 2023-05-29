016cd1512aefd53e446bcddd17d678afc9432a99
Adrien Loup <adloup@gmail.com>
2023-05-29 19:49:56 +0200

    Initialize lcc project

--- src/pages/Questions/Page/Page.tsx
+++ src/pages/Questions/Page/Page.tsx
@@ -6,6 +6,7 @@ import Scorebar from '../../../components/molecules/Scorebar/Scorebar'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
+import Back from '../../../components/molecules/Back/Back'
 import Title from '../../../components/atoms/Title/Title'
 
 function Page() {
@@ -34,35 +35,34 @@ function Page() {
       >
         <Scorebar />
         <article className='article'>
-          <Title text='6/8 Quel est le nombre de pages consultées&nbsp;?' />
+          <div className='heading'>
+            <Title text='6/8 Quel est le nombre de pages consultées&nbsp;?' />
+          </div>
           <Notebook note={'Le parcours utilisateur, après l’identification du besoin utilisateurs, est le deuxième point crutial en éco-conception. Plus le parcours, entre l’utilisateur et le contenu ou le service pour lequel il est venu, est plus faible réduit l’impact écologique. En 2021, le poids moyen d’une page web est de 2,1Mo (contre 0,013Mo en 1995).'} />
-          <form className='form' action=''>
-            <label htmlFor='page'>{value} {value > 1 ? 'pages' : 'page'}</label>
-            <input
-              id='page'
-              type='range'
-              min='1'
-              max='10'
-              step='1'
-              style={{backgroundSize: `${(value * 100) / 10}% 100%`}}
-              value={value}
-              onChange={handleChange}
-            />
-          </form>
+          <div className='frame'>
+            <form className='form' action=''>
+              <label htmlFor='page'>{value} {value > 1 ? 'pages' : 'page'}</label>
+              <input
+                id='page'
+                type='range'
+                min='1'
+                max='10'
+                step='1'
+                style={{backgroundSize: `${(value * 100) / 10}% 100%`}}
+                value={value}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
           <nav
-            className='navigation'
+            className='footing'
             role='navigation'
           >
+            <Back />
             <Button
-              label='Retour'
+              label='Je continue'
               cssClass='button'
-              ariaLabel={'Retour à la question précédente'}
-              click={() => navigate(-1)}
-            />
-            <Button
-              label='Continuer'
-              cssClass='button'
-              ariaLabel='Continuer le calcul'
+              ariaLabel='Je continue le calcul'
               click={() => handleClick()}
             />
           </nav>
