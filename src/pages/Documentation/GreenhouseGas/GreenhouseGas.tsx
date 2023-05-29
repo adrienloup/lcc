c5e847ee3c23787591b89e360624fed38b0e9aed
Adrien Loup <adloup@gmail.com>
2023-05-29 19:49:23 +0200

    Initialize lcc project

--- src/pages/Documentation/GreenhouseGas/GreenhouseGas.tsx
+++ src/pages/Documentation/GreenhouseGas/GreenhouseGas.tsx
@@ -1,21 +1,41 @@
-import { useNavigate } from 'react-router-dom'
 import { useContext, useEffect } from 'react'
 import { DataContext } from '../../../contexts/data'
+import gas from '../../../utils/gas'
+import meter from '../../../utils/meter'
+import car from '../../../utils/car'
 import Heading from '../../../components/molecules/Heading/Heading'
 import Subtitle from '../../../components/atoms/Subtitle/Subtitle'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
-import Button from '../../../components/molecules/Button/Button'
 import Score from '../../../components/molecules/Score/Score'
+import Back from '../../../components/molecules/Back/Back'
 import Text from '../../../components/atoms/Text/Text'
-import gas from '../../../utils/gas'
 
 function GreenhouseGas() {
-  const navigate = useNavigate()
   const { data } = useContext(DataContext)
 
   const gasScore = gas(
-    data.visitor,
+    data.user,
+    data.duration,
+    data.userlocated,
+    data.mobile,
+    data.page,
+    data.server,
+    data.serverlocated
+  )
+
+  const meterScore = meter(
+    data.user,
+    data.duration,
+    data.userlocated,
+    data.mobile,
+    data.page,
+    data.server,
+    data.serverlocated
+  )
+
+  const carScore = car(
+    data.user,
     data.duration,
     data.userlocated,
     data.mobile,
@@ -39,20 +59,18 @@ function GreenhouseGas() {
           <Heading text='Gaz à effet de serre' />
           <Text text='<p>Voici votre score provisoire, il évolue à chaque nouvelle réponse !</p>' />
           <Score text={`${gasScore} tonnes de CO2-eq`} icon='cloud' />
-          <Score text={`46254 voitures par année`} icon='car' />
-          <Score text={`1542 mètres en voiture par utilisateur`} icon='map' />
+          <Score text={`${carScore} voitures par année`} icon='car' />
+          <Score text={`${meterScore} mètres en voiture par utilisateur`} icon='map' />
+          <Subtitle text='Impacts écologiques par année' />
           <Subtitle text='Comment cette donnée est-elle calculée&nbsp;?' />
           <Text text='<p>Si vous êtes développeur/euse vous trouverez ci-dessous des informations techniques utiles pour l’intégration de cette règle dans votre application.</p>' />
           <Subtitle text='Informations techniques' />
           <Text text='<p>Si vous êtes développeur/euse vous trouverez ci-dessous des informations techniques utiles pour l’intégration de cette règle dans votre application.</p>' />
           <Subtitle text='Informations techniques' />
           <Text text='<p>Si vous êtes développeur/euse vous trouverez ci-dessous des informations techniques utiles pour l’intégration de cette règle dans votre application.</p>' />
-          <Button
-            label='Page précédente'
-            cssClass='button'
-            ariaLabel={'Page précédente'}
-            click={() => navigate(-1)}
-          />
+          <div className='footing'>
+            <Back />
+          </div>
         </article>
       </main>
       <Footer />
