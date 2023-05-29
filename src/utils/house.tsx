7c6681fa891a5b7d54214673ecea72034f90453d
Adrien Loup <adloup@gmail.com>
2023-05-29 19:49:26 +0200

    Initialize lcc project

---
+++ src/utils/house.tsx
@@ -0,0 +1,14 @@
+function house(
+  user: number,
+  duration: number,
+  mobile: number,
+  server: number
+) {
+  return Math.round((
+    (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 75 + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * 30 + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 30.8
+    + (((user * 12 * duration) / 60) / (24 * 365)) * 150
+    + (((450 * 365 * 24) / 1000) * (60 / 100)) * server * 1.8
+  ) / 4679)
+}
+
+export default house
