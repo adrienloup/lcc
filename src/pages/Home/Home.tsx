2df70eb47f85bdd84ba97ad558cff1737c259f2b
Adrien Loup <adloup@gmail.com>
2023-05-29 19:49:18 +0200

    Initialize lcc project

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,4 @@
 import Scorebar from '../../components/molecules/Scorebar/Scorebar'
-import Subtitle from '../../components/atoms/Subtitle/Subtitle'
 import Header from '../../components/organisms/Header/Header'
 import Footer from '../../components/organisms/Footer/Footer'
 import Button from '../../components/molecules/Button/Button'
@@ -17,22 +16,19 @@ function Home() {
         <Scorebar />
         <article className='article'>
           <div className='heading'>
-            <Title text='Quel est l’impact de votre site Web<br />sur la planète&nbsp;?' />
+            <Title text='Quel est l’impact écologique de votre site Web sur la planète&nbsp;?' />
           </div>
           <div className='frame'>
-            <Text text='<p>Le calculateur de cycle de vie est un outil d’éco-conception et d’Analyse de Cycle de Vie (ACV), qui vous aide à éco-concevoir votre site Web.</p><p>Répondez à 8 questions pour évaluer votre empreinte carbone. Votre résultat évolue à chaque nouvelle réponse. Vous pouvez modifier vos réponses pour améliorer le score. Chaque calcul est sauvegardé dans votre navigateur Web. Vous êtes le seul à y avoir accès.</p>' />
+            <Text text='<p>Le calculateur de cycle de vie est un outil d’éco-conception et d’Analyse de Cycle de Vie (ACV), qui vous aide à éco-concevoir votre site Web.</p><p>Répondez à 8 questions pour évaluer l’impact de votre site Web. Votre résultat évolue à chaque nouvelle réponse. Vous pouvez modifier les réponses pour améliorer votre résultat. Chaque calcul est sauvegardé dans votre navigateur Web. Vous êtes le seul à y avoir accès.</p>' />
           </div>
-          <nav
-            className='wrapper'
-            role='navigation'
-          >
+          <div className='footing'>
             <Button
-              label='À l’action !'
+              label='Passez à l’action !'
               cssClass='start'
-              ariaLabel='À l’action pour commencer le calcul'
+              ariaLabel='Passez à l’action pour commencer le calcul'
               to='/lcc/question/site-web'
             />
-          </nav>
+          </div>
         </article>
       </main>
       <Footer />
