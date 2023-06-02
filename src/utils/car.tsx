d4e8867a96b69504512822641deec9793c16eb9b
Adrien Loup <adloup@gmail.com>
2023-06-02 05:35:37 +0200

    Initialize lcc project

--- src/utils/car.tsx
+++ src/utils/car.tsx
@@ -8,12 +8,12 @@ function car(
   page: number,
   server: number,
   serverlocated: string
-) {
-  return Math.round(((
-    (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 75 * energetic(userlocated) + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * 30 * energetic(userlocated) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 30.8 * energetic(userlocated) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (189 / 5) + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * (169 / 4) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (265 / 6)
-    + (((user * 12 * duration) / 60) / (24 * 365)) * 150 * energetic(userlocated) + (((user * 12 * duration) / 60) / (24 * 365)) * (83 / 5) + user * 12 * page * 2.1 / 1024 * (0.002)
-    + (((450 * 365 * 24) / 1000) * 0.6) * server * 1.8 * energetic(serverlocated) + server * (756 / 5)
-  ) / 0.1) / 1000).toFixed(0)
+  ) {
+    return Math.round(((
+      ((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 75 * energetic(userlocated) + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * 30 * energetic(userlocated) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * 30.8 * energetic(userlocated) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (189 / 5) + (((user * 12 * duration) / 60) * (mobile / 100)) / (365 * 4) * (169 / 4) + (((user * 12 * duration) / 60) * (1 - (mobile / 100))) / (220 * 7) * (265 / 6)
+      + (((user * 12 * duration) / 60) / (24 * 365)) * 150 * energetic(userlocated) + (((user * 12 * duration) / 60) / (24 * 365)) * (83 / 5) + user * 12 * page * 2.1 / 1024 * (0.002)
+      + (((450 * 365 * 24) / 1000) * 0.6) * server * 1.8 * energetic(serverlocated) + server * (756 / 5)
+    ) / 0.1)
 }
 
 export default car
