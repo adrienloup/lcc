ca5e9e99543501d74dcd740cc1e56a691056c30d
Adrien Loup <adloup@gmail.com>
2023-06-05 18:40:18 +0200

    Initialize lcc project

--- src/pages/Questions/Page/Page.tsx
+++ src/pages/Questions/Page/Page.tsx
@@ -37,7 +37,7 @@ function Page() {
           <div className='heading'>
             <Title text='6/8 Quel est le nombre de pages consultées&nbsp;?' />
           </div>
-          <Notebook note={'Le parcours utilisateur, après l’identification du besoin utilisateurs, est le deuxième point crutial en éco-conception. Plus le parcours, entre l’utilisateur et le contenu ou le service pour lequel il est venu, est plus faible réduit l’impact écologique. En 2021, le poids moyen d’une page web est de 2,1Mo (contre 0,013Mo en 1995).'} />
+          <Notebook note={'Le parcours utilisateur, après l’identification du besoin utilisateurs, est le deuxième point crutial en éco-conception. Plus le parcours, entre l’utilisateur et le contenu ou le service pour lequel il est venu, est plus faible, plus il réduit l’impact écologique. En 2021, le poids moyen d’une page web est de 2,1Mo (contre 0,013Mo en 1995).'} />
           <div className='frame'>
             <form className='form' action=''>
               <label htmlFor='page'>{value} {value > 1 ? 'pages' : 'page'}</label>
