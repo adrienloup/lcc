47c9da26a5a84cef6f83291bd2d5ece86d028e20
Adrien Loup <adloup@gmail.com>
2023-06-05 18:40:21 +0200

    Initialize lcc project

--- src/components/atoms/Percent/Percent.tsx
+++ src/components/atoms/Percent/Percent.tsx
@@ -1,10 +1,10 @@
 import { PercentProps } from '../../../models/percent'
 import './Percent.scss'
 
-function Percent({ number }: PercentProps) {
+function Percent({ percent }: PercentProps) {
   return (
-    <div className={`percent ${(number > 0) ? 'positive' : 'negative'}`}>
-      {(number > 0) ? `+${(number).toFixed(0)}` : (number).toFixed(0)}%
+    <div className={`percent ${(percent > 0) ? 'positive' : 'negative'}`}>
+      {(percent > 0) ? `+${(percent).toFixed(0)}` : (percent).toFixed(0)} %
     </div>
   )
 }
