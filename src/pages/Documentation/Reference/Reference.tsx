ce4c13a05a2966ae34155c6d130a6ff88916e9e8
Adrien Loup <adloup@gmail.com>
2023-06-05 18:41:05 +0200

    Initialize lcc project

--- src/pages/Documentation/Reference/Reference.tsx
+++ src/pages/Documentation/Reference/Reference.tsx
@@ -5,7 +5,7 @@ import Subtitle from '../../../components/atoms/Subtitle/Subtitle'
 import Header from '../../../components/organisms/Header/Header'
 import Footer from '../../../components/organisms/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
-import List from '../../../components/molecules/List/List'
+import List from '../../../components/atoms/List/List'
 import Text from '../../../components/atoms/Text/Text'
 
 function Reference() {
@@ -44,7 +44,7 @@ function Reference() {
         <article className='article'>
           <Heading text='Les modèles de référence' />
           <div className='frame'>
-            <Text text='<p>Le calculateur de cycle de vie évalue l’impact annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
+            <Text text='<p>Le calculateur de cycle de vie évalue l’impact écologique annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
           </div>
           <div className='frame'>
             <Subtitle text='Comment l’impact écologique est-il calculé&nbsp;?' />
