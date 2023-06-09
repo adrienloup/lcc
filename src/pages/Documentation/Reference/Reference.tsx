2980b7f1540f11d03c189c7e9817bdc9ee75848c
Adrien Loup <adloup@gmail.com>
2023-06-09 22:33:40 +0200

    Initialize lcc project

--- src/pages/Documentation/Reference/Reference.tsx
+++ src/pages/Documentation/Reference/Reference.tsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react'
 import { models } from '../../../datas/models.json'
 import Heading from '../../../components/molecules/Heading/Heading'
 import Subtitle from '../../../components/atoms/Subtitle/Subtitle'
-import Header from '../../../components/organisms/Header/Header'
-import Footer from '../../../components/organisms/Footer/Footer'
+import Header from '../../../components/molecules/Header/Header'
+import Footer from '../../../components/molecules/Footer/Footer'
 import Button from '../../../components/molecules/Button/Button'
 import List from '../../../components/atoms/List/List'
 import Text from '../../../components/atoms/Text/Text'
@@ -44,7 +44,8 @@ function Reference() {
         <article className='article'>
           <Heading text='Les modèles de référence' />
           <div className='frame'>
-            <Text text='<p>Le calculateur de cycle de vie évalue l’impact écologique annuelle de votre site Web. L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
+            <Text text='<p>Le calculateur de cycle de vie évalue l’impact écologique annuelle du parcours d’un utilisateur de votre site Web.</p>
+              <p>L’outil utilise plusieurs métriques : trafic du site Web, intensité énergétique, intensité carbone de l’électricité, source d’énergie utilisée.</p>' />
           </div>
           <div className='frame'>
             <Subtitle text='Comment l’impact écologique est-il calculé&nbsp;?' />
