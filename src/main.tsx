d572280520fb403ba2785f4000a84a8adf0764db
Adrien Loup <adloup@gmail.com>
2023-06-09 22:31:27 +0200

    Initialize lcc project

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './styles/app.scss'
 
 console.log(
   '%c[LIFE CYCLE CALCULATOR]%cv1.0.3%c[@jff]',
-  'padding: 4px 3px 3px; background: #32337b; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
+  'padding: 4px 3px 3px; background: #fff; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;',
   'padding: 4px 3px 3px; background: #38dbf3; font-weight: bold; color: #32337b;',
   'padding: 4px 3px 3px; background: #32337b; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;'
 );
