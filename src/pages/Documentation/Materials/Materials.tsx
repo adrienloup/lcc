c3eb48032836ef27aca1f7d588e5bc4accf7da35
Adrien Loup <adloup@gmail.com>
2023-07-14 04:31:07 +0200

    materials page added

--- src/pages/Documentation/Materials/Materials.tsx
+++ src/pages/Documentation/Materials/Materials.tsx
@@ -61,7 +61,13 @@ function Materials() {
             <Subtitle text='Informations techniques' />
             <Text text='<p>Parce qu’1 KWh produit en Chine par une centrale à charbon émet 1Kg de CO2-eq, soit 10x plus qu’en France par une centrale nucléaire et 5x plus en moyenne qu’un pays de l’OCDE.</p>' />
             <Subtitle text='Comment cette donnée est-elle calculée&nbsp;?' />
-            <Text text='<p>L’analyse du cycle de vie est une méthode d’évaluation normalisée permettant de réaliser un bilan environnemental multicritère et multi-étape d’un système sur l’ensemble de son cycle de vie.</p>' />
+            <Text text='
+              <p>L’analyse du cycle de vie est une méthode d’évaluation normalisée permettant de réaliser un bilan environnemental multicritère et multi-étape d’un système sur l’ensemble de son cycle de vie.</p>
+              <p>Le nombre de visiteurs par mois permet de connaître la quantité de teminaux utilisateur, équipements réseau, serveur nécessaire pour consulter les contenus ou les services que vous délivrez. Si vous ne la connaissez pas, vous pouvez trouver cette information dans votre outil Google Analytics ou équivalent.</p>
+              <p>Le temps que les utilisateurs passent sur votre service numérique permet de connaître la quantité de teminaux, équipements réseau, serveur nécessaire pour servir, transiter et consulter les contenus ou les services que vous délivrez.</p>
+              <p>Les conditions d’utilisation de votre service numérique, en mobilité ou au bureau, ont des impacts bien différents.</p>
+              <p>Calculé à partir d’un PUE (Power Usage Effectiveness) moyen d’un serveur (soit 1,8). Si vous avez opté pour un hébergement « green », vous serez meilleur que la moyenne du marché sur ce paramètre. Compter les serveurs de production, staging, review, CDN, etc...</p>
+            ' />
             <Warning text='<p>Information bientôt disponible</p>' />
           </div>
           <div className='footing'>
